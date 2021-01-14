01fa1b66cb06cc018768e64e126cca85aa5deee5
Joel Joseph <joeljordanjoseph@gmail.com>
2021-01-14 13:31:53 -0700

    Add files via upload

---
+++ SwiftUI-MapKit/SwiftUI-MapKit/SwiftUI_MapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MapKitApp.swift
+//  SwiftUI-MapKit
+//
+//  Created by Joel Joseph on 1/14/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
